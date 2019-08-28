527205f32abcd33799a6efc617c3818886e83e6e
root <root@ip-172-31-43-209.us-east-2.compute.internal>
2019-08-28 00:33:08 +0000

    all files

---
+++ guttikonda/src/main/java/dwaraka/App.java
@@ -0,0 +1,13 @@
+package dwaraka;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
